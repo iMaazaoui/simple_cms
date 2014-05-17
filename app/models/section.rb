05af09d538b3b664b50ed0c53f99ad320aafe7ad
iMaazaoui <maazaoui.imed@gmail.com>
2014-05-17 12:37:13 +0100

    Working on validations

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,6 +4,21 @@ class Section < ActiveRecord::Base
   has_many :section_edits
   has_many :editors, :through => :section_edits, :class_name => "AdminUser"
 
+  CONTENT_TYPES = ['text', 'HTML']
+
+  # validates_presence_of :name
+  # validates_length_of :name, :maximum => 255
+  # validates_inclusion_of :content_type, :in CONTENT_TYPES,
+  # 	:message => "must be one of: #{CONTENT_TYPES.join(', ')}"
+  # validates_presence_of :content 
+
+  # shortcut validations, aka "sexy validations"
+  validates :name, :presence => true,
+  								 :length => { :maximum => 25 }
+  validates :content_type, :inclusion => { :in => CONTENT_TYPES, 
+  	:message => "must be one of: #{CONTENT_TYPES.join(', ')}"}
+  validates :content, :presence => true
+
   scope :visible, lambda { where(:visible => true) }
   scope :invisible, lambda { where(:visible => false) }
   scope :sorted, lambda { order("sections.position ASC") }
